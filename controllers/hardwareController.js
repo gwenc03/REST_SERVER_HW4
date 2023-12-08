e8d499e9a8ecdbd99fef62b8f3282f2a1e594e9d
ashnakashima <nakashlyn@gmail.com>
2023-12-08 13:04:15 -0600

    deleted stuff we didn't need

--- controllers/hardwareController.js
+++ controllers/hardwareController.js
@@ -2,57 +2,18 @@ const Customer = require ("../models/customer");
 const Item = require("../models/item");
 const Sale = require ("../models/sale")
 
-// exports.getAddCustomer = ( req, res, next) => {
-//     res.render( 'hardware/addCustomer',
-//         {
-//             from: 'addCustomer'
-//         })
-// }
-// exports.getAddItem = ( req, res, next) => {
-//     res.render( 'hardware/addItem',
-//         {
-//             from: 'addItem'
-//         })
-// }
-// exports.postAddItem = ( req, res, next) => {
-//     let name = req.body.name;
-//     let price = req.body.price;
-//     let iid = Item.count() + 1;
-//     const item = new Item(iid, name, price);
-//     item.save();
-//     res.redirect('/showItems');
-// }
-// exports.postAddCustomer = ( req, res, next) => {
-//     let name = req.body.name;
-//     let email = req.body.email;
-//     let cid = Customer.count() + 1;
-//     const customer = new Customer(cid, name, email);
-//     customer.save();
-//     res.redirect('/showCustomers');
-// }
+
 exports.getCustomers = (req, res, next) => {
-    // Customer.runCustomerQuery()
     Customer.fetchAll()
         .then((rows, fieldData ) => {
             console.log( "ROws="); console.log( rows );
-            // res.render("hardware/showCustomersAdmin", {
-            //     title: "Customer Info",
-            //     from: "showCustomers",
-            //     customers: rows[0]
-            // })
             res.status(200).json( rows[0]);
         })
 }
 exports.getItems = (req, res, next) => {
-    // Item.runItemQuery()
     Item.runItemQuery()
         .then((rows, fieldData ) => {
             console.log( "ROws="); console.log( rows );
-            // res.render("hardware/showItemAdmin", {
-            //     title: "Item Info",
-            //     from: "showItems",
-            //     items: rows[0]
-            // })
             res.status(200).json( rows[0]);
         })
 }
@@ -85,15 +46,9 @@ exports.getCustomerDetails = ( req, res, next ) => {
     })
 }
 exports.getSales = (req, res, next) => {
-    // Item.runItemQuery()
     Sale.runSaleQuery()
         .then((rows, fieldData ) => {
             console.log( "ROws="); console.log( rows );
-            // res.render("hardware/showItemAdmin", {
-            //     title: "Item Info",
-            //     from: "showItems",
-            //     items: rows[0]
-            // })
             res.status(200).json( rows[0]);
         })
 }
@@ -168,7 +123,6 @@ exports.postUpdatedItem = (req, res, next) => {
     let name = req.body.ItemName;
     let price = req.body.ItemPrice;
     const item = new Item(id, name, price);
-    // console.log("in postupdateditem:" + id)
     item.update()
         .then(() => {
             res.status(200).json({message: 'item updated '});
@@ -191,65 +145,3 @@ exports.postUpdatedCustomer = (req, res, next) => {
         res.status(500).json({error: `Failed to update customer; ${err.message}`});
     });
 }
-//
-// exports.getSales = (req, res, next) => {
-//     Sale.runSaleQuery()
-//         .then((rows, fieldData ) => {
-//             res.render("hardware/showSalesAdmin", {
-//                 title: "Sales Info",
-//                 from: "showSales",
-//                 sales: rows[0]
-//             })
-//         })
-// }
-//
-// exports.editCustomer = ( req, res, next ) => {
-//     let id = req.params.cid;
-//     Customer.findById(id)
-//         .then ((rows, fieldData) =>{
-//             res.render( 'hardware/ShowUpdateForm', {
-//                 title : `Update record:${id} `,
-//                 id : rows[0].id,
-//                 from: 'updateProducts',
-//                 customer: rows[0][0]
-//             })
-//         }).catch( err => {
-//         console.log( "DB Error=>");
-//         console.log( err );
-//     })
-// }
-//
-// exports.postUpdateCustomer = ( req, res, next ) => {
-//     let id = req.body.CustomerID;
-//     let name = req.body.CustomerName;
-//     let email = req.body.CustomerEmail;
-//     const customer = new Customer(id, name, email);
-//     customer.update(id).then((rows, fieldData) => {
-//         res.redirect('/showCustomers')
-//     }).catch(err => {
-//         console.log('WTH');
-//         console.log(err);
-//     })
-// }
-//
-// exports.getHomePage = ( req, res, next) => {
-//     let topCustomers, topItems, topSales;
-//     Customer.runTop5CustomerQuery().then(([customerData]) => {
-//         topCustomers = customerData;
-//         return Item.runTop5ItemQuery();
-//     }).then(([itemData]) => {
-//         topItems = itemData;
-//         return Sale.runTopSalesQuery();
-//     }).then(([saleData]) => {
-//         topSales = saleData;
-//         res.render("hardware/home", {
-//             from: "home",
-//             customer: topCustomers,
-//             item: topItems,
-//             sale: topSales
-//         })
-//     }).catch(err => {
-//         console.log('No Home Page');
-//         console.log(err);
-//     })
-// }
--- routes/hardware.js
+++ routes/hardware.js
@@ -4,27 +4,20 @@ const path = require('path');
 const hardwareController = require("../controllers/hardwareController")
 
 router.get( '/customers', hardwareController.getCustomers);
+router.get('/customers/:id', hardwareController.getCustomerDetails);
+router.put('/customers/:id', hardwareController.postUpdatedCustomer);
+router.delete('/customers/:id', hardwareController.deleteCustomer);
+router.post('/customers', hardwareController.postNewCustomer);
+
 router.get('/items', hardwareController.getItems);
 router.get( '/items/:id', hardwareController.getItemDetails);
-router.get('/sales', hardwareController.getSales);
-router.get('/customers/:id', hardwareController.getCustomerDetails);
-router.delete('/customers/:id', hardwareController.deleteCustomer);
+router.put('/items/:id', hardwareController.postUpdatedItem);
 router.delete('/items/:id', hardwareController.deleteItem);
 router.post('/items', hardwareController.postNewItem);
-router.post('/customers', hardwareController.postNewCustomer);
+
+router.get('/sales', hardwareController.getSales);
+
 router.get('/homepage', hardwareController.getHomePage);
-router.put('/items/:id', hardwareController.postUpdatedItem);
-router.put('/customers/:id', hardwareController.postUpdatedCustomer);
 
-// router.get('/items/:id', hardwareController.getUpdatedItem);
-
-// router.get('/showSales', hardwareController.getSales);
-// router.get('/editCustomer/:cid', hardwareController.editCustomer);
-// router.get('/addCustomer', hardwareController.getAddCustomer);
-// router.post('/customer', hardwareController.postAddCustomer);
-// router.get('/addItem', hardwareController.getAddItem);
-// router.post('/item', hardwareController.postAddItem);
-// router.post('/postUpdateCustomer', hardwareController.postUpdateCustomer);
-// router.get('/home', hardwareController.getHomePage);
 
 exports.routes = router;
